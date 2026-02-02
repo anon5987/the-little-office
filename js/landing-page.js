36b232547be419dde9ba8835f9a9453994293da0
Jan Švec <jan.svec@memos.cz>
2026-02-02 22:08:02 +0100

    add buy me a coffee

--- js/landing-page.js
+++ js/landing-page.js
@@ -137,6 +137,12 @@ export function renderLandingPage() {
           <li>${getHourNameTranslated('matins', lang)}</li>
         </ol>
       </div>
+
+      <div class="bmc-container">
+        <a href="https://www.buymeacoffee.com/jan99" target="_blank" class="bmc-button">
+          <img src="https://cdn.buymeacoffee.com/buttons/v2/default-blue.png" alt="Buy Me A Coffee" />
+        </a>
+      </div>
     </div>
   `;
 
