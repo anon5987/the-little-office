187293d3c2d1f3c89eadce2e95419365f6591142
Jan Švec <jan.svec@memos.cz>
2026-02-07 21:36:12 +0100

    fix typo

--- data/gabc/common/opening.js
+++ data/gabc/common/opening.js
@@ -12,7 +12,7 @@ export default {
 (c3)De(h)us,(h_) <i>✠</i> (,) in(h) ad(h)ju(h)tó(hi)ri(h)um(h) me(h)um(h_) in(h)tén(gh)de.(h.) (::)
 Dó(h)mi(h)ne,(h_) (,) ad(h) ad(h)ju(h)ván(h)dum(h) me(h_) fe(h)stí(gh)na.(h) (:)
 Gló(h)ri(h)a(h) Pá(h)tri,(h) et(h) Fí(h)li(h)o,(h_) (,) et(h) Spi(h)rí(h)tu(h)i(h) Sán(gh)cto.(h.) (:)
-Si(h)cut(h) é(h)rat(h) in(h) prin(h)ce(h)í(h)pi(h)o,(h) et(h) nunc,(h) et(h) sem(h)per,(h.) (,) et(h) in(h) sæ(h)cu(h)la(h) sæ(h)cu(h)ló(h)rum.(h) 
+Si(h)cut(h) é(h)rat(h) in(h) prin(h)cí(h)pi(h)o,(h) et(h) nunc,(h) et(h) sem(h)per,(h.) (,) et(h) in(h) sæ(h)cu(h)la(h) sæ(h)cu(h)ló(h)rum.(h) 
 A(gh)men.(h.) (:)
 Al(h)le(i)lú(hg~)ia.(g.) <i>Or:</i> (::)
 Laus(h) tí(h)bi(h) Dó(h)mi(h)ne,(h_) Rex(h) æ(h)ter(h)ne(i) gló(h)ri(h)æ.(g.) (::)`,
